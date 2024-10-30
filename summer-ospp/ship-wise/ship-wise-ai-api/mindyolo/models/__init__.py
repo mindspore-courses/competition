f17b3f9313939becf0313c4207ccc57af98bdbdd
Jiang Liu <emailtojiang@gmail.com>
2024-10-30 10:22:18 +0800

    提交比赛相关文件

--- summer-ospp/ship-wise/ship-wise-ai-api/mindyolo/models/__init__.py
+++ summer-ospp/ship-wise/ship-wise-ai-api/mindyolo/models/__init__.py
@@ -1,17 +1,15 @@
-from . import (heads, initializer, layers, losses, model_factory, yolov3,
-               yolov4, yolov5, yolov7, yolov8)
+from . import (heads, initializer, layers, losses, model_factory)
+
+from . import shipwise
 
 __all__ = []
+
 __all__.extend(heads.__all__)
 __all__.extend(layers.__all__)
 __all__.extend(losses.__all__)
-__all__.extend(yolov8.__all__)
-__all__.extend(yolov7.__all__)
-__all__.extend(yolov5.__all__)
-__all__.extend(yolov4.__all__)
-__all__.extend(yolov3.__all__)
 __all__.extend(initializer.__all__)
 __all__.extend(model_factory.__all__)
+__all__.extend(shipwise.__all__)
 
 # fixme: since yolov7 is used as both the file and function name, we need to import * after __all__
 
@@ -20,9 +18,4 @@ from .initializer import *
 from .layers import *
 from .losses import *
 from .model_factory import *
-from .yolov3 import *
-from .yolov4 import *
-from .yolov5 import *
-from .yolov7 import *
-from .yolov8 import *
-from .yolox import *
+from .shipwise import *
