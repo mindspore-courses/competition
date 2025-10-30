3c7dbada918e7054aca1bca17613807840bc84b7
guolei666 <1359677949@qq.com>
2025-10-30 11:16:45 +0800

    Delete tts.py

--- tts.py
+++
@@ -1,9 +0,0 @@
-# tts.py
-import base64
-from huaweicloud_sis import tts_text_to_wav
-
-def tts_wav_bytes(text: str) -> bytes:
-    return tts_text_to_wav(text, lang="en_us")
-
-def tts_wav_base64(text: str) -> str:
-    return base64.b64encode(tts_wav_bytes(text)).decode("utf-8")
